10a5f9b0ca6226e7adfdebf26293f860d64c47ab
Andrea <andrea.berrocal95@gmail.com>
2023-08-11 13:43:47 -0700

    add constants

---
+++ src/components/utils/feedContansts.js
@@ -0,0 +1,15 @@
+export const NAME = 'Andrea Berrocal';
+
+export const USER_NAME = '@Andrea_Berrocal@dev.social';
+
+export const IMAGE = '/AndreaBerrocal.png';
+
+export const SUMMARY = 'Junior Software Engineer @ American Express';
+
+export const PROFILE_STATS = {
+    'cats' : 2,
+    'followers': 1000,
+    'following': 1000,
+};
+
+export const FEED_SECTIONS = ['Posts', 'Projects', 'About', 'Contact'];
